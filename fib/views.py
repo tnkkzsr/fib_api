2b7523c504e58d407754e6f5b399aef6f48db385
TanakaKazushiro <tnk30125146@gmail.com>
2023-10-09 15:30:49 +0900

    fib

--- fib/views.py
+++ fib/views.py
@@ -14,12 +14,12 @@ class TestView(APIView):
 
 
 def fib(n):
-    if n <= 1:
-            return n
-    a, b = 0, 1
-    for _ in range(2, n + 1):
-        a, b = b, a + b
-    return b
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
 
 class FibView(APIView):
     def get(self,request):
